070187d98a08e66aef3f9f4b37a8ee160a18a354
bravocode <david.alfonso.hdz.05@gmail.com>
2025-05-29 03:15:54 -0400

    feat(Pincipal): crear ventanas multiples y tabla perzonalizada para mostrar datos

---
+++ src/frontend/VentanaVisitas.java
@@ -0,0 +1,33 @@
+package frontend;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class VentanaVisitas extends JPanel {
+
+	public VentanaVisitas() {
+		JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setBackground(Color.WHITE);
+		panelPrincipal.setBounds(0, 0, 832, 689);
+		add(panelPrincipal);
+		panelPrincipal.setLayout(null);
+		
+		JPanel panelSuperior = new JPanel();
+		panelSuperior.setBounds(0, 0, 874, 51);
+		panelPrincipal.add(panelSuperior);
+		panelSuperior.setBackground(new Color(0, 171, 227));
+		panelSuperior.setLayout(null);
+		
+		JLabel cartelPestanna = new JLabel("VISITAS");
+		cartelPestanna.setHorizontalAlignment(SwingConstants.LEFT);
+		cartelPestanna.setForeground(Color.WHITE);
+		cartelPestanna.setFont(new Font("Arial", Font.PLAIN, 18));
+		cartelPestanna.setBounds(25, 0, 107, 51);
+		panelSuperior.add(cartelPestanna);
+	}
+
+}
