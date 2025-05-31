a917d09810158beb68f22df170bdd7aecedb7116
reynaldo.dqg <reynaldo.dqg@avangenio.com>
2025-05-30 22:51:36 -0500

    fix(runner): delete unnecesary login method

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -24,13 +24,4 @@ public class Runner {
 			System.exit(0);
 		}
 	}
-	
-	public boolean authUser(String user, String password) {
-	    boolean response = false;
-	 
-	    if(user.equals("1") && password.equals("1")){
-	    	response = true;
-	    }
-		return response;
-	}
 }
