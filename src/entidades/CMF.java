0f4ecc16e0d272d479f6357c5dee2737424acac9
reynaldo.dqg <reynaldo.dqg@avangenio.com>
2025-06-02 18:08:04 -0500

    feat(cmf): add mock data

--- src/entidades/CMF.java
+++ src/entidades/CMF.java
@@ -141,19 +141,51 @@ public class CMF {
 	}
 
 	public void cargarDatos() {
-		crearMedico("Alfonso Rodriguez Camela", 11321, "98020378176", "12/3/1998");
+	    crearMedico("Alfonso Rodriguez Camela", 11321, "98020378176", "12/3/1998");
 
-		ArrayList<String> vacunacion1 = new ArrayList<>();
-		vacunacion1.add("Antiplio: 13/4/2009");
-		agregarPaciente(1, "Armando Lopez Del Toro", 45, vacunacion1);
+	    ArrayList<String> vacunacion = new ArrayList<String>();
+	    vacunacion.add("Antiplio: 13/4/2009");
+	    agregarPaciente(1, "Armando Lopez Del Toro", 45, vacunacion);
 
-		ArrayList<String> vacunacion2 = new ArrayList<>();
-		vacunacion2.add("Antiplio: 13/4/2020");
-		agregarPaciente(2, "Amanda Gutierres Garcia", 25, vacunacion2);
+	    ArrayList<String> vacunacion2 = new ArrayList<String>();
+	    vacunacion2.add("Antiplio: 13/4/2020");
+	    agregarPaciente(2, "Amanda Lopez Garcia", 25, vacunacion2);
 
-		ArrayList<String> vacunacion3 = new ArrayList<>();
-		vacunacion3.add("Antiplio: 13/4/2019");
-		agregarPaciente(3, "Carlos Garces Fernandez", 34, vacunacion3);
+	    ArrayList<String> vacunacion3 = new ArrayList<String>();
+	    vacunacion3.add("Antiplio: 13/4/2019");
+	    agregarPaciente(3, "Carlos Garces Fernandez", 34, vacunacion3);
+
+	    ArrayList<String> vacunacion4 = new ArrayList<String>();
+	    vacunacion4.add("Antiplio: 10/1/2022");
+	    vacunacion4.add("Antitetánica: 5/3/2023");
+	    agregarPaciente(4, "Daniela Suarez Molina", 29, vacunacion4);
+
+	    ArrayList<String> vacunacion5 = new ArrayList<String>();
+	    vacunacion5.add("Antiplio: 15/5/2021");
+	    vacunacion5.add("Antigripal: 20/10/2022");
+	    agregarPaciente(5, "Esteban Pérez López", 52, vacunacion5);
+
+	    agregarPaciente(6, "Fernando Gómez Rivas", 40, new ArrayList<>(java.util.Arrays.asList("Antigripal: 12/9/2022")));
+	    agregarPaciente(7, "Gabriela Torres Martínez", 31, new ArrayList<>(java.util.Arrays.asList("Antitetánica: 3/4/2021")));
+	    agregarPaciente(8, "Héctor Sánchez López", 28, new ArrayList<>(java.util.Arrays.asList("Antiplio: 15/2/2018")));
+	    agregarPaciente(9, "Isabel Fernández Cruz", 37, new ArrayList<>(java.util.Arrays.asList("Antigripal: 11/11/2020", "Antitetánica: 20/7/2022")));
+	    agregarPaciente(10, "Javier Morales Castillo", 50, new ArrayList<>(java.util.Arrays.asList("Antiplio: 2/6/2015")));
+	    agregarPaciente(11, "Karla Ruiz Domínguez", 26, new ArrayList<>(java.util.Arrays.asList("Antigripal: 8/10/2019")));
+	    agregarPaciente(12, "Luis Herrera Pérez", 33, new ArrayList<>(java.util.Arrays.asList("Antitetánica: 14/1/2020")));
+	    agregarPaciente(13, "María José Salazar", 45, new ArrayList<>(java.util.Arrays.asList("Antigripal: 17/3/2021")));
+	    agregarPaciente(14, "Nicolás Vega Ortega", 39, new ArrayList<>(java.util.Arrays.asList("Antiplio: 5/12/2017")));
+	    agregarPaciente(15, "Olga Díaz García", 41, new ArrayList<>(java.util.Arrays.asList("Antigripal: 22/8/2020")));
+	    agregarPaciente(16, "Pablo Martínez Sánchez", 34, new ArrayList<>(java.util.Arrays.asList("Antitetánica: 19/5/2019")));
+	    agregarPaciente(17, "Quetzal Rojas Castillo", 29, new ArrayList<>(java.util.Arrays.asList("Antigripal: 13/7/2021")));
+	    agregarPaciente(18, "Raúl López Fernández", 43, new ArrayList<>(java.util.Arrays.asList("Antiplio: 30/3/2016")));
+	    agregarPaciente(19, "Sofía Medina Ramos", 27, new ArrayList<>(java.util.Arrays.asList("Antigripal: 9/9/2022")));
+	    agregarPaciente(20, "Tomás Aguilar Herrera", 38, new ArrayList<>(java.util.Arrays.asList("Antitetánica: 1/2/2020")));
+	    agregarPaciente(21, "Úrsula Vargas Delgado", 32, new ArrayList<>(java.util.Arrays.asList("Antiplio: 23/11/2018")));
+	    agregarPaciente(22, "Víctor Salinas Mora", 47, new ArrayList<>(java.util.Arrays.asList("Antigripal: 29/6/2019")));
+	    agregarPaciente(23, "Wendy Cruz López", 35, new ArrayList<>(java.util.Arrays.asList("Antitetánica: 7/8/2021")));
+	    agregarPaciente(24, "Ximena Flores Castillo", 30, new ArrayList<>(java.util.Arrays.asList("Antigripal: 14/4/2022")));
+	    agregarPaciente(25, "Yahir Castillo Gómez", 44, new ArrayList<>(java.util.Arrays.asList("Antiplio: 6/10/2014")));
+	    agregarPaciente(26, "Zulema Navarro Ruiz", 36, new ArrayList<>(java.util.Arrays.asList("Antigripal: 18/1/2020")));
 	}
 
 	public int obtenerTotalPacientes(){
