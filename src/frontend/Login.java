2a217d50030ac959391f38747c8044039ffe92e2
bravocode <david.alfonso.hdz.05@gmail.com>
2025-06-03 21:29:53 -0400

    feat(app): cambios pequeños de interfaz

--- src/frontend/Login.java
+++ src/frontend/Login.java
@@ -1,66 +1,37 @@
 package frontend;
 
-import java.awt.BorderLayout;
 import java.awt.Cursor;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.Window.Type;
-
-import javax.swing.JTable;
-import javax.swing.JComboBox;
-import javax.swing.JEditorPane;
 
 import java.awt.Color;
 
-import javax.swing.JDesktopPane;
-import javax.swing.BoxLayout;
-
-import java.awt.GridLayout;
-
 import javax.swing.JOptionPane;
-import javax.swing.JToolBar;
-import javax.swing.JTabbedPane;
-import javax.swing.JButton;
-import javax.swing.JMenu;
 import javax.swing.JLabel;
 
 import java.awt.Toolkit;
-import java.awt.Canvas;
 
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 
 import java.awt.Font;
-import java.awt.Dialog.ModalExclusionType;
-
-import javax.swing.JCheckBox;
-import javax.swing.UIManager;
-
 import java.awt.SystemColor;
-import java.awt.Button;
 
 import javax.swing.JSeparator;
 
-import java.awt.Panel;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.SwingConstants;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.IOException;
-
 import javax.swing.JPasswordField;
 
+import componentesPropios.BotonBlanco;
+
 import runner.Auth;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 	private boolean contrasenaVisible;
@@ -81,18 +52,18 @@ public class Login extends JFrame {
 		setTitle("Autenticaci\u00F3n");
 		setBounds(100, 100, 800, 510);
 		getContentPane().setLayout(null);
+		setLocationRelativeTo(null);
 
-
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 0, 794, 534);
-		getContentPane().add(panel);
-		panel.setLayout(null);
+		JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setBackground(Color.WHITE);
+		panelPrincipal.setBounds(0, 0, 794, 534);
+		getContentPane().add(panelPrincipal);
+		panelPrincipal.setLayout(null);
 
 		JPanel panelLateral = new JPanel();
 		panelLateral.setBackground(new Color(0, 171, 227));
 		panelLateral.setBounds(0, 0, 250, 600);
-		panel.add(panelLateral);
+		panelPrincipal.add(panelLateral);
 		panelLateral.setLayout(null);
 
 		JPanel panelLogo = new JPanel();
@@ -101,20 +72,20 @@ public class Login extends JFrame {
 		panelLogo.setBackground(new Color(109, 163, 67));
 		panelLogo.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/fotos/Tipografia-Peque.png")));
-		lblNewLabel.setBounds(67, 129, 112, 62);
-		panelLogo.add(lblNewLabel);
+		JLabel imagenTipografia = new JLabel();
+		imagenTipografia.setIcon(new ImageIcon(Login.class.getResource("/fotos/Tipografia-Peque.png")));
+		imagenTipografia.setBounds(67, 129, 112, 62);
+		panelLogo.add(imagenTipografia);
 
 		JLabel cartelLogo = new JLabel("");
 		cartelLogo.setBounds(67, 15, 112, 120);
 		panelLogo.add(cartelLogo);
 		cartelLogo.setIcon(new ImageIcon(Login.class.getResource("/fotos/Logo peque.png")));
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(21, 15, 208, 189);
-		panelLogo.add(panel_1);
+		JPanel panelBlancoLogo = new JPanel();
+		panelBlancoLogo.setBackground(Color.WHITE);
+		panelBlancoLogo.setBounds(21, 15, 208, 189);
+		panelLogo.add(panelBlancoLogo);
 
 		JPanel panelSeparadorLogo = new JPanel();
 		panelSeparadorLogo.setBackground(Color.WHITE);
@@ -125,13 +96,13 @@ public class Login extends JFrame {
 		ojoIcono.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if (!contrasenaVisible && !campoContrasenna.getText().equals("\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF")) {
+                if (!contrasenaVisible && !campoContrasenna.getText().equals("\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022")) {
                 	ojoIcono.setIcon(new ImageIcon(Login.class.getResource("/fotos/visible (1).png")));
             		campoContrasenna.setEchoChar((char) 0);  
             		contrasenaVisible = true;
                 } else {
                 	ojoIcono.setIcon(new ImageIcon(Login.class.getResource("/fotos/no visible (1).png")));
-                	campoContrasenna.setEchoChar('*');  
+                	campoContrasenna.setEchoChar('\u2022');  
                 	contrasenaVisible = false;
                 }
             }
@@ -139,13 +110,13 @@ public class Login extends JFrame {
 		ojoIcono.setBackground(SystemColor.menu);
 		ojoIcono.setIcon(new ImageIcon(Login.class.getResource("/fotos/no visible (1).png")));
 		ojoIcono.setBounds(671, 281, 33, 32);
-		panel.add(ojoIcono);
+		panelPrincipal.add(ojoIcono);
 		ojoIcono.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
 		JLabel cartelUsuario = new JLabel("USUARIO");
-		cartelUsuario.setFont(new Font("Arial", Font.BOLD, 18));
+		cartelUsuario.setFont(new Font("Arial", Font.PLAIN, 18));
 		cartelUsuario.setBounds(325, 139, 92, 26);
-		panel.add(cartelUsuario);
+		panelPrincipal.add(cartelUsuario);
 
 		campoUsuario = new JTextField();
 		campoUsuario.addFocusListener(new FocusListener() {
@@ -153,6 +124,7 @@ public class Login extends JFrame {
                 if (campoUsuario.getText().equals("Ingrese el nombre de usuario")) {
                 	campoUsuario.setText("");
                 	campoUsuario.setForeground(Color.BLACK);
+                	campoUsuario.setFont(new Font("Arial", Font.PLAIN, 16));
                 }
             }
 
@@ -160,6 +132,7 @@ public class Login extends JFrame {
                 if (campoUsuario.getText().isEmpty()) {
                 	campoUsuario.setForeground(Color.LIGHT_GRAY);
                 	campoUsuario.setText("Ingrese el nombre de usuario");
+                	campoUsuario.setFont(new Font("Arial", Font.ITALIC, 16));
                 }
             }
         });
@@ -168,19 +141,19 @@ public class Login extends JFrame {
 		campoUsuario.setFont(new Font("Arial", Font.ITALIC, 16));
 		campoUsuario.setBackground(Color.WHITE);
 		campoUsuario.setBounds(325, 169, 342, 32);
-		panel.add(campoUsuario);
+		panelPrincipal.add(campoUsuario);
 		campoUsuario.setColumns(10);
 		campoUsuario.setBorder(null);
 
 		JLabel cartelContrasenna = new JLabel("CONTRASE\u00D1A");
-		cartelContrasenna.setFont(new Font("Arial", Font.BOLD, 18));
+		cartelContrasenna.setFont(new Font("Arial", Font.PLAIN, 18));
 		cartelContrasenna.setBounds(325, 249, 144, 26);
-		panel.add(cartelContrasenna);
+		panelPrincipal.add(cartelContrasenna);
 
 		campoContrasenna = new JPasswordField();
 		campoContrasenna.addFocusListener(new FocusListener() {
             public void focusGained(FocusEvent e) {
-                if (campoContrasenna.getText().equals("\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF")) {
+                if (campoContrasenna.getText().equals("\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022")) {
                 	campoContrasenna.setText("");
                 	campoContrasenna.setForeground(Color.BLACK);
                 }
@@ -189,75 +162,58 @@ public class Login extends JFrame {
 			public void focusLost(FocusEvent e) {
                 if (campoContrasenna.getText().isEmpty()) {
                 	campoContrasenna.setForeground(Color.LIGHT_GRAY);
-                	campoContrasenna.setText("\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF");
+                	campoContrasenna.setText("\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022");
                 }
             }
         });
 		campoContrasenna.setForeground(SystemColor.controlShadow);
-		campoContrasenna.setText("\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF\u25CF");
-		campoContrasenna.setFont(new Font("Arial", Font.ITALIC, 16));
+		campoContrasenna.setText("\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022");
+		campoContrasenna.setFont(new Font("Arial", Font.PLAIN, 16));
 		campoContrasenna.setBackground(Color.WHITE);
 		campoContrasenna.setBounds(325, 281, 342, 32);
-		panel.add(campoContrasenna);
+		panelPrincipal.add(campoContrasenna);
 		campoContrasenna.setColumns(10);
 		campoContrasenna.setBorder(null);
 
 		JLabel cartelIniciarSesion = new JLabel("INICIAR SESI\u00D3N");
 		cartelIniciarSesion.setFont(new Font("Arial Black", Font.BOLD, 21));
 		cartelIniciarSesion.setBounds(325, 82, 210, 26);
-		panel.add(cartelIniciarSesion);
+		panelPrincipal.add(cartelIniciarSesion);
 
 		JSeparator separatorUsuario = new JSeparator();
 		separatorUsuario.setBackground(SystemColor.menu);
 		separatorUsuario.setBounds(325, 202, 379, 2);
-		panel.add(separatorUsuario);
+		panelPrincipal.add(separatorUsuario);
 
 		JSeparator separatorContrasenna = new JSeparator();
 		separatorContrasenna.setBackground(SystemColor.menu);
 		separatorContrasenna.setBounds(325, 315, 379, 2);
-		panel.add(separatorContrasenna);
+		panelPrincipal.add(separatorContrasenna);
 
-		final Panel botonAceptar = new Panel();
+		final BotonBlanco botonAceptar = new BotonBlanco("ACEPTAR");
+		botonAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String usuario = campoUsuario.getText();
+		    	String contrasenna = new String(campoContrasenna.getPassword());
+
+		    	Auth authManager = new Auth();
+
+		    	try {
+		    		if (authManager.authUser(usuario, contrasenna)) {
+		    			auth = true;
+		    			dispose(); 
+		    		}
+		    	} catch (Auth.AuthenticationException ex) {
+		    		JOptionPane.showMessageDialog(null, ex.getMessage(), "Error de autenticación", JOptionPane.ERROR_MESSAGE);
+		    	}
+			}
+		});
+		botonAceptar.setForeground(new Color(0, 0, 0));
 		botonAceptar.setBackground(SystemColor.menu);
-		botonAceptar.setBounds(325, 361, 115, 43);
-		panel.add(botonAceptar);
+		botonAceptar.setBounds(325, 361, 129, 43);
+		panelPrincipal.add(botonAceptar);
 		botonAceptar.setLayout(null);
 
-		JLabel cartelAceptar = new JLabel("ACEPTAR");
-		cartelAceptar.addMouseListener(new MouseAdapter() {
-	    @Override
-	    public void mouseEntered(MouseEvent arg0) {
-	        botonAceptar.setBackground(Color.LIGHT_GRAY);
-	    }
-	
-	    @Override
-	    public void mouseExited(MouseEvent arg0) {
-	        botonAceptar.setBackground(SystemColor.menu);
-	    }
-	
-	    @Override
-	    public void mouseClicked(MouseEvent e) {
-	        String usuario = campoUsuario.getText();
-	        String contrasenna = new String(campoContrasenna.getPassword());
-
-	        Auth authManager = new Auth();
-
-	        try {
-	            if (authManager.authUser(usuario, contrasenna)) {
-	                auth = true;
-	                dispose(); 
-	            }
-	        } catch (Auth.AuthenticationException ex) {
-	            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error de autenticación", JOptionPane.ERROR_MESSAGE);
-	        }
-	    }
-
-	});
-		cartelAceptar.setHorizontalAlignment(SwingConstants.CENTER);
-		cartelAceptar.setBounds(0, 0, 115, 43);
-		botonAceptar.add(cartelAceptar);
-		cartelAceptar.setFont(new Font("Arial", Font.BOLD, 18));
-		cartelAceptar.setCursor(new Cursor(Cursor.HAND_CURSOR));
 	}
 	
 	public boolean getAuth(){
