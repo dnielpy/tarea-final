52ec793ea7e1fee640cf96797bbe217a0bfd5da9
reynaldo.dqg <reynaldo.dqg@avangenio.com>
2025-06-17 17:19:04 -0500

    feat(login): fixed

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,12 +1,11 @@
 package runner;
 
+import entidades.CMF;
 import frontend.Login;
 import frontend.Principal;
 
-public class Runner {
-
-    public void run() {
-        // Mostrar login modal
+public class Runner {	
+    public static void login() {
         Login login = new Login(null);
         login.setVisible(true);
 
@@ -15,9 +14,22 @@ public class Runner {
             Principal principal = new Principal(); // Tu única clase principal
             principal.setVisible(true);
         } else {
-            System.exit(0); // Salir si no se autenticó
+            login.setVisible(true); // Salir si no se autenticó
+        }
+    }
+
+    public static void logout() {
+    	CMF cmf = CMF.getInstance();
+    	cmf.setUsuario(null);
+        Login login = new Login(null);
+        login.setVisible(true);
+
+        // Si se autenticó correctamente, abrir ventana principal
+        if (login.authenticado()) {
+            Principal principal = new Principal(); // Tu única clase principal
+            principal.setVisible(true);
+        } else {
+            login.setVisible(true); // Salir si no se autenticó
         }
     }
 }
-
-
